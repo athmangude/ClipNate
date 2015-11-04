f4f501196e2a48f269723e4ebdb8b261c9c775e5
Athman Gude <athmangude@gmail.com>
2015-11-04 07:09:11 +0300

    Created Feed module

---
+++ Feed.js
@@ -0,0 +1,29 @@
+'user strict';
+
+// Bringing in React Native
+var React = require('react-native');
+
+// Bringing in useful components
+var {
+    Component,
+    Text,
+    View
+} = React;
+
+class Feed extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    this.state = {
+
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+module.exports = Feed;
